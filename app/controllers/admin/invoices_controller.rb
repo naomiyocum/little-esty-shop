1e0743d935a43d49868b3a0f19e40c7c5af2de96
Krisetn Nestler <kristen.nestler@gmail.com>
2022-11-05 18:32:15 -0400

    Admin Invoice Show Page item info revenue and update status

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -5,6 +5,16 @@ class Admin::InvoicesController < ApplicationController
   end
 
   def show
+    @invoices = Invoice.all
     @invoice = Invoice.find(params[:id])
+  end
+
+  def update
+    invoice = Invoice.find(params[:id])
+
+    invoice_item = InvoiceItem.where(invoice_id: invoice.id, item_id: params[:item_id]).first
+    invoice_item.update(status: params[:status])
+
+    redirect_to admin_invoice_path(invoice)
   end
 end
