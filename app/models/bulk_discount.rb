a0b1394568fd834d6b39ec378a0cc733059583c2
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-11 10:23:14 -0700

    create max_discount method

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,4 +3,10 @@ class BulkDiscount < ApplicationRecord
 
   validates :percentage_discount, :quantity_threshold, :presence => true
   validates :percentage_discount, :quantity_threshold, :numericality => true
+
+  def self.max_discount(quantity)
+    where("quantity_threshold <= ?", quantity)
+      .order(percentage_discount: :desc)
+      .first
+  end
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,35 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  let!(:merchant_1) {Merchant.create!(name: "Shawn LLC")}
-  let!(:merchant_2) {Merchant.create!(name: "Naomi LLC")}
-  let!(:merchant_3) {Merchant.create!(name: "Kristen LLC")}
-  let!(:merchant_4) {Merchant.create!(name: "Yuji LLC")}
-  let!(:merchant_5) {Merchant.create!(name: "Turing LLC")}
+  let!(:merch_1) {create(:merchant)}
 
-  let!(:customer_1) {Customer.create!(first_name: "Sally", last_name: "Shopper")}
-  let!(:customer_2) {Customer.create!(first_name: "Evan", last_name: "East")}
-  let!(:customer_3) {Customer.create!(first_name: "Yasha", last_name: "West")}
-  let!(:customer_4) {Customer.create!(first_name: "Du", last_name: "North")}
-  let!(:customer_5) {Customer.create!(first_name: "Polly", last_name: "South")}
+  let!(:bulk_1) {create(:bulk_discount, merchant: merch_1, quantity_threshold: 5, percentage_discount: 5)}
+  let!(:bulk_2) {create(:bulk_discount, merchant: merch_1, quantity_threshold: 10, percentage_discount: 10)}
+  let!(:bulk_3) {create(:bulk_discount, merchant: merch_1, quantity_threshold: 20, percentage_discount: 20)}
 
-  let!(:item_1) {Item.create!(name: "Anime Stickers", description: "Random One Piece and Death Note stickers", unit_price: 500, merchant_id: merchant_1.id)}
-  let!(:item_2) {Item.create!(name: "Lava Lamp", description: "Special blue/purple wax inside a glass vessel", unit_price: 2000, merchant_id: merchant_2.id)}
-  let!(:item_3) {Item.create!(name: "Orion Flag", description: "A flag of Okinawa's most popular beer", unit_price: 850, merchant_id: merchant_3.id)}
+  let!(:customer_1) {create(:customer)}
+  let!(:customer_2) {create(:customer)}
+  let!(:customer_3) {create(:customer)}
 
-  let!(:invoice_1) {Invoice.create!(status: "in progress", customer_id: customer_1.id)}
-  let!(:invoice_2) {Invoice.create!(status: "cancelled", customer_id: customer_2.id)}
-  let!(:invoice_3) {Invoice.create!(status: "completed", customer_id: customer_3.id)}
-  let!(:invoice_4) {Invoice.create!(status: "in progress", customer_id: customer_4.id)}
-  let!(:invoice_5) {Invoice.create!(status: "in progress", customer_id: customer_4.id)}
-  let!(:invoice_6) {Invoice.create!(status: "in progress", customer_id: customer_4.id)}
+  let!(:invoice_1) {customer_1.invoices.create!(status: 2, created_at: Time.parse("22.11.03"))}
+  let!(:invoice_2) {create(:invoice, customer: customer_1)}
 
-  let!(:invoice_items_1) {InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 2, unit_price: 11, status: "shipped")}
-  let!(:invoice_items_2) {InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_2.id, quantity: 2, unit_price: 11, status: "packaged")}
-  let!(:invoice_items_3) {InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_3.id, quantity: 2, unit_price: 11, status: "pending")}
-  let!(:invoice_items_4) {InvoiceItem.create!(item_id: item_1.id, invoice_id: invoice_4.id, quantity: 2, unit_price: 11, status: "packaged")}
-  let!(:invoice_items_5) {InvoiceItem.create!(item_id: item_2.id, invoice_id: invoice_5.id, quantity: 2, unit_price: 11, status: "pending")}
-  let!(:invoice_items_6) {InvoiceItem.create!(item_id: item_3.id, invoice_id: invoice_6.id, quantity: 2, unit_price: 11, status: "shipped")}
+  let!(:invoice_item_1) {create(:invoice_item, invoice: invoice_1, item: item_1, quantity: 10, status: 0)}
+  let!(:invoice_item_2) {create(:invoice_item, invoice: invoice_1, item: item_2, quantity: 5)}
+  let!(:invoice_item_3) {create(:invoice_item, invoice: invoice_1, item: item_3, quantity: 1)}
+  let!(:invoice_item_4) {create(:invoice_item, invoice: invoice_1, item: item_4, quantity: 12)}
+
+  let!(:item_1) {create(:item, merchant: merch_1)}
+  let!(:item_2) {create(:item, merchant: merch_1)}
+  let!(:item_3) {create(:item, merchant: merch_1)}
+  let!(:item_4) {create(:item, merchant: merch_1)}
 
   describe 'relationships' do
     it {should belong_to(:invoice)}
@@ -45,10 +38,15 @@ RSpec.describe InvoiceItem, type: :model do
   end
 
   describe 'model tests' do
-
     describe '#uniq_invoice_items' do
       it 'returns a unique list of invoice items' do
-        expect(InvoiceItem.uniq_invoice_items.count).to eq(6)
+        expect(InvoiceItem.uniq_invoice_items.count).to eq(4)
+      end
+    end
+
+    describe '#max_discount' do
+      it 'returns the highest discount the invoice_item qualifies for' do
+        expect(invoice_item_1.max_discount). to eq(bulk_2)
       end
     end
   end
