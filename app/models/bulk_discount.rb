7e5a4076855cc6dfd5b95ed22dfa2f95903c47a1
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-15 16:19:59 -0700

    add validation for percentage to be greater than or equal to 1

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,7 +2,7 @@ class BulkDiscount < ApplicationRecord
   belongs_to :merchant
 
   validates :percentage_discount, :quantity_threshold, :presence => true
-  validates :percentage_discount, :numericality => { :less_than_or_equal_to => 100 }
+  validates :percentage_discount, :numericality => { :less_than_or_equal_to => 100, :greater_than_or_equal_to => 1 }
   validates :quantity_threshold, :numericality => { :greater_than_or_equal_to => 1}
 
   def self.max_discount(quantity)
