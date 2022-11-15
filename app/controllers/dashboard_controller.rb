d845f1e440c1dfdaca8725e3623fb3faa4a87e39
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-15 11:32:21 -0700

    Add sad path testing when searching for Merchant

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,8 +1,13 @@
 class DashboardController < ApplicationController
   def index
     if params[:search]
-      @merchant = Merchant.find(params[:search])
-      redirect_to merchant_dashboard_index_path(@merchant)
+      if Merchant.find_by(id: params[:search]) != nil
+        @merchant = Merchant.find(params[:search])
+        redirect_to merchant_dashboard_index_path(@merchant)
+      else
+        redirect_to root_path
+        flash[:alert] = "Merchant ##{params[:search]} Does Not Exist"
+      end
     else
       @merchant = Merchant.find(params[:merchant_id])
     end
