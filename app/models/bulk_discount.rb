6d4b91c2125895c9b5410cce647e8a178f0e5769
Naomi Yocum <naomiyocum24@gmail.com>
2022-11-15 15:18:36 -0700

    add numericality validations

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -2,7 +2,8 @@ class BulkDiscount < ApplicationRecord
   belongs_to :merchant
 
   validates :percentage_discount, :quantity_threshold, :presence => true
-  validates :percentage_discount, :quantity_threshold, :numericality => true
+  validates :percentage_discount, :numericality => { :less_than_or_equal_to => 100 }
+  validates :quantity_threshold, :numericality => { :greater_than_or_equal_to => 1}
 
   def self.max_discount(quantity)
     where("quantity_threshold <= ?", quantity)
